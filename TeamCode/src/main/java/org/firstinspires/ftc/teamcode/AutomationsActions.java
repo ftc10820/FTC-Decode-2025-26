fc1c902222d29ea2cd988adf8e2b882778b23aad
Chinmay M <chinmay.malvania@gmail.com>
2025-11-22 17:56:41 -0500

    Implement automation actions and refactor OpModes
    This commit introduces modular robot actions, refactors the Red Goal autonomous routine, and creates a new TeleOp mode.
    
    ### Automation and Infrastructure
    - Created `AutomationsActions` class to centralize robot behaviors.
    - Implemented `Shooter` class for flywheel control.
    - Implemented `Transfer` class with a state machine for servo operation.
    - Implemented `HuskyLensDriveControl` and `AutoAlignGoal` to detect AprilTags (ID 21 for Red, 24 for Blue) and strafe to the target using RoadRunner.
    
    ### Autonomous Changes (`RedGoalAuto`)
    - Refactored to use RoadRunner's `SequentialAction`.
    - Updated sequence to spin up the shooter, drive to the launch zone, and execute the auto-alignment action.
    
    ### TeleOp Changes
    - **`teleop`:** Swapped `gamepad1` X and Y stick inputs to correct drive orientation and added manual transfer servo toggling.
    - **`TeleopActions`:** Added a new TeleOp OpMode that utilizes the `AutomationsActions` class for shooter and transfer sequences.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutomationsActions.java
@@ -0,0 +1,180 @@
+package org.firstinspires.ftc.teamcode;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.huskylens.HuskyLensCam;
+import org.firstinspires.ftc.teamcode.huskylens.ObjectInfo;
+
+import java.util.Objects;
+
+public class AutomationsActions {
+    private enum TransferState {
+        INITIAL,
+        POS_1,
+        WAITING,
+        POS_0,
+        FINISHED
+    }
+    public class Shooter {
+        private final DcMotorEx motor;
+
+        public Shooter(HardwareMap hardwareMap) {
+            motor = hardwareMap.get(DcMotorEx.class, "flywheel");
+        }
+
+        public class SpinUp implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    motor.setDirection(DcMotorEx.Direction.REVERSE);
+                    motor.setPower(0.8);
+                    initialized = true;
+                }
+
+                double vel = motor.getVelocity();
+                packet.put("shooterVelocity", vel);
+                return vel < 10_000.0;
+            }
+        }
+
+        public Action spinUp() {
+            return new SpinUp();
+        }
+
+    }
+
+
+
+        public class Transfer {
+            private final Servo servo;
+
+            public Transfer(HardwareMap hardwareMap) {
+                servo = hardwareMap.get(Servo.class, "transfer");
+
+            }
+
+
+            public class DoTransfer implements Action {
+                private final ElapsedTime timer = new ElapsedTime();
+                private TransferState currentState = TransferState.INITIAL;
+                private static final double WAIT_TIME = 1.0;
+
+
+
+
+                @Override
+                public boolean run(@NonNull TelemetryPacket packet) {
+                    switch (currentState) {
+                        case INITIAL:
+                            servo.setPosition(1);
+                            currentState = TransferState.POS_1;
+                            return true;
+
+                        case POS_1:
+                            servo.setPosition(0);
+                            timer.reset();
+                            currentState = TransferState.WAITING;
+                            return true;
+
+                        case WAITING:
+
+                            if (timer.seconds() >= WAIT_TIME) {
+                                currentState = TransferState.POS_0;
+                            }
+                            return true;
+
+                        case POS_0:
+                            servo.setPosition(1);
+                            currentState = TransferState.FINISHED;
+                            return false;
+
+                        case FINISHED:
+                            return false;
+                    }
+                    return true;
+                }
+            }
+
+            public Action doTransfer() {
+                return new DoTransfer();
+            }
+        }
+    public class HuskyLensDriveControl {
+        private final HuskyLensCam Cam;
+        private ObjectInfo goalTag;
+        private final String Alliance;
+        private final MecanumDrive Drive;
+
+
+        public HuskyLensDriveControl(HuskyLensCam cam, MecanumDrive drive, String alliance) {
+            Cam = cam;
+            if (!alliance.equalsIgnoreCase("red") && !alliance.equalsIgnoreCase("blue")){
+                throw new IllegalArgumentException("Invalid alliance: " + alliance+ "\n use red or blue");
+            }
+            Alliance = alliance.toLowerCase();
+            Drive = drive;
+        }
+
+        //TODO: change this name to be more practical
+        public class AutoAlignGoal implements Action {
+            private boolean initialized = false;
+            private Action trajectoryAction;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    if (Objects.equals(Alliance, "red")) {
+                        for (ObjectInfo o : Cam.scanTag()) {
+                            // TODO: Get real tag id for red
+                            if (Objects.equals(o.objectName, "apriltag id #" + 21)) {
+                                goalTag = o;
+                                break;
+                            }
+                        }
+                    } else {
+                        for (ObjectInfo o : Cam.scanTag()) {
+                            // TODO: Get real tag id for blue
+                            if (Objects.equals(o.objectName, "apriltag id #" + 24)) {
+                                goalTag = o;
+                                break;
+                            }
+                        }
+                    }
+
+
+
+                    //code goes here
+                    if (goalTag != null) {
+                        Pose2d targetPose = Cam.getPoseOf(Drive.localizer.getPose(), goalTag);
+                        trajectoryAction = Drive.actionBuilder(Drive.localizer.getPose())
+                                .strafeToLinearHeading(new Vector2d(targetPose.position.x+10, targetPose.position.y+10), targetPose.heading)
+                                .build();
+                    }
+                    initialized = true;
+
+                }
+                if (trajectoryAction != null) {
+                    return trajectoryAction.run(packet);
+                }
+                return false;
+            }
+        }
+
+        public Action autoAlignGoal() {
+            return new AutoAlignGoal();
+        }
+
+    }
+
+    }
