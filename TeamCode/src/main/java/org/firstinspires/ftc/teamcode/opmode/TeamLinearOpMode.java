03fe83812bec53cda4c83fd92dc07f7817dc8889
Coach Weston <deacent@att.net>
2026-02-20 22:57:08 -0500

    Squashed commit of the following:
    commit 88c79ad837936011dd0b2989fb99f66818ca5629
    Merge: c633c18 61fd41b
    Author: Coach Weston <deacent@att.net>
    Date:   Fri Feb 20 22:54:57 2026 -0500
    
        Merge branch 'refactorOpModes' of https://github.com/ftc10820/FTC-Decode-2025-26 into refactorOpModes
    
    commit c633c18614cbbd3b618af445293c8c4149ee8a56
    Author: Coach Weston <deacent@att.net>
    Date:   Fri Feb 20 19:10:08 2026 -0500
    
        Refactor OpModes to use abstract TeamLinearOpMode and abstract TeamTeleopMode to unify initialization and camera set up
    
        Unified action member names and initialization
    
        Fixed typos
    
        Removed unused imports
    
        Merged new changes into unified initialization
    
        Refactor OpModes to use abstract TeamLinearOpMode and abstract TeamTeleopMode to unify initialization and camera set up
    
        Unified action member names and initialization
    
        Fixed typos
    
        Removed unused imports
    
        Merge
    
        Merge in latest changes
    
    commit 61fd41b4334bd88c82aaa7110f1d8a64620ae35f
    Author: Coach Weston <deacent@att.net>
    Date:   Fri Feb 20 22:36:02 2026 -0500
    
        Merge
    
    commit d1e8dc4983e6b9d8124d8736afe1e5cfa79cab73
    Merge: 47e1511 7c6d6b3
    Author: Coach Weston <deacent@att.net>
    Date:   Fri Feb 20 22:35:32 2026 -0500
    
        Merge branch 'refactorOpModes' of https://github.com/ftc10820/FTC-Decode-2025-26 into refactorOpModes
    
    commit 47e1511e801417b97fce61ace9a446f813da61c2
    Author: Coach Weston <deacent@att.net>
    Date:   Fri Feb 20 22:33:05 2026 -0500
    
        Merged new changes into unified initialization
    
    commit e908f56747e7e8a3231c34c7a84bbee01f3e22d5
    Author: Coach Weston <deacent@att.net>
    Date:   Fri Feb 20 20:39:25 2026 -0500
    
        Removed unused imports
    
    commit c503c57c857918eeb8e03f57e0f39cf5d1bcd29d
    Author: Coach Weston <deacent@att.net>
    Date:   Fri Feb 20 20:33:23 2026 -0500
    
        Fixed typos
    
    commit 665522faf114734af20dc9cf69e978b50a860cf3
    Author: Coach Weston <deacent@att.net>
    Date:   Fri Feb 20 20:32:50 2026 -0500
    
        Unified action member names and initialization
    
        # Conflicts:
        #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/RedFarLaunchZoneAUTO.java
    
    commit 101a3179b00d056d1c672195aed90d5078a99d15
    Author: Coach Weston <deacent@att.net>
    Date:   Fri Feb 20 19:10:08 2026 -0500
    
        Refactor OpModes to use abstract TeamLinearOpMode and abstract TeamTeleopMode to unify initialization and camera set up
    
    commit 7c6d6b3d1440b391cc04d29684e6bf19d2a4b082
    Author: Coach Weston <deacent@att.net>
    Date:   Fri Feb 20 20:39:25 2026 -0500
    
        Removed unused imports
    
    commit d172f37126850a5bba032301254c4fa73f6455f9
    Author: Coach Weston <deacent@att.net>
    Date:   Fri Feb 20 20:33:23 2026 -0500
    
        Fixed typos
    
    commit 202567d66bc594fb133225892bd3c9d72e0b3eae
    Author: Coach Weston <deacent@att.net>
    Date:   Fri Feb 20 20:32:50 2026 -0500
    
        Unified action member names and initialization
    
    commit d4ddfb885acf8789ca5ab9b7cf805129ac238b8a
    Author: Coach Weston <deacent@att.net>
    Date:   Fri Feb 20 19:10:08 2026 -0500
    
        Refactor OpModes to use abstract TeamLinearOpMode and abstract TeamTeleopMode to unify initialization and camera set up

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeamLinearOpMode.java
@@ -0,0 +1,159 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.AutomationsActions;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.camera.limelight.LimelightCam;
+
+public abstract class TeamLinearOpMode extends LinearOpMode {
+    public DcMotor intake = null;
+
+    boolean useIntake = false;
+    boolean useFlywheel = false;
+    boolean useTransfer = false;
+    boolean useDrivetrain = false;
+    boolean useColorSensor = false;
+    public Servo transfer = null;
+    public Servo transfer2 = null;
+    public Servo transfer3 = null;
+
+    // drive train motors
+    public DcMotor frontLeft;
+    public DcMotor frontRight;
+    public DcMotor backLeft;
+    public DcMotorEx flywheel;
+    public DcMotor backRight;
+    MecanumDrive drive;
+    public ColorSensor colorSensor1;
+    public ColorSensor colorSensor2;
+    public ColorSensor colorSensor3;
+    public AutomationsActions.CamControl camControl;
+    public AutomationsActions.Transfer transferControl;
+    public AutomationsActions.Shooter shooterControl;
+    public AutomationsActions.Intake intakeControl;
+    public AutomationsActions.HuskyLensServo hlservo;
+    public Servo LED;
+    public final double TICKS_PER_REV = 28;
+    public final double FLYWHEEL_RPM = 2700;
+    public final double FLYWHEEL_TICKS_PER_REV = TICKS_PER_REV * FLYWHEEL_RPM / 60.0;
+    public boolean isUseCam = false;
+
+    // there are specific ways that the drive power is calculated based on automations
+    double frontLeftPower = 0.0, backLeftPower = 0.0, frontRightPower = 0.0, backRightPower = 0.0;
+
+    //private IntegratingGyroscope NavX;
+    // crane linear slide and lifter
+
+    // helper variables
+    ElapsedTime eTime1 = new ElapsedTime();
+    ElapsedTime eTime2 = new ElapsedTime();
+
+    ElapsedTime eTeleOp = new ElapsedTime();
+    AutomationsActions.BallColor[] shootingOrder;
+
+    double speedFactor = 1;
+
+    public abstract void runOpMode() throws InterruptedException;
+
+    public void initialize() {
+
+        // setting up drive train
+        try {
+            frontRight = hardwareMap.get(DcMotorEx.class, "rightFront");
+            frontLeft = hardwareMap.get(DcMotorEx.class, "leftFront");
+            backRight = hardwareMap.get(DcMotorEx.class, "rightBack");
+            backLeft = hardwareMap.get(DcMotorEx.class, "leftBack");
+            frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+            useDrivetrain = true;
+            telemetry.addData("Debug", "drivetrain detected, proceeding with");
+
+        } catch (Exception e) {
+            telemetry.addData("Debug", "no drivetrain detected, proceeding without");
+
+        }
+        try{
+            flywheel = hardwareMap.get(DcMotorEx.class,"flywheel");
+            useFlywheel = true;
+            telemetry.addData("Debug", "flywheel detected, proceeding with");
+
+        }
+        catch (Exception e) {
+            telemetry.addData("Debug", "no flywheel detected, proceeding without");
+
+        }
+        try{
+            intake = hardwareMap.get(DcMotor.class, "intake");
+            useIntake = true;
+            telemetry.addData("Debug", "intake detected, proceeding with");
+
+        } catch (Exception e) {
+            telemetry.addData("Debug", "no intake detected, proceeding without");
+
+        }
+        try{
+            transfer = hardwareMap.get(Servo.class,"transfer");
+            transfer2 = hardwareMap.get(Servo.class,"transfer2");
+            transfer3 = hardwareMap.get(Servo.class,"transfer3");
+            useTransfer = true;
+            telemetry.addData("Debug", "transfer detected, proceeding with");
+
+        } catch (Exception e) {
+            telemetry.addData("Debug", "no transfer detected, proceeding without");
+
+        }
+        try {
+            colorSensor1 = hardwareMap.get(ColorSensor.class, "colorSensor1");
+            colorSensor2 = hardwareMap.get(ColorSensor.class, "colorSensor2");
+            colorSensor3 = hardwareMap.get(ColorSensor.class, "colorSensor3");
+
+            useColorSensor = true;
+            telemetry.addData("Debug", "color sensor detected, proceeding with. 1: "+colorSensor1.getI2cAddress()+" 2: "+colorSensor2.getI2cAddress()+" 3: "+colorSensor3.getI2cAddress());
+
+
+        } catch (Exception e) {
+            telemetry.addData("Debug", "no color sensor detected, proceeding without");
+
+        }
+        try{
+            AutomationsActions actions = new AutomationsActions();
+            drive = new MecanumDrive(hardwareMap,new Pose2d(0,0,0));
+            camControl =  actions.new CamControl(new LimelightCam(hardwareMap.get(Limelight3A.class, "limelight"),316.9,  41.91, 9),drive,"red");
+            transferControl = actions.new Transfer(hardwareMap, drive);
+            shooterControl = actions.new Shooter(hardwareMap);
+            intakeControl = actions.new Intake(hardwareMap);
+            hlservo = actions.new HuskyLensServo(hardwareMap);
+            LED = hardwareMap.get(Servo.class, "LED1");
+
+            isUseCam = true;
+            telemetry.addData("Debug", "cam detected, proceeding with");
+
+        } catch (Exception e) {
+            telemetry.addData("Debug", "no cam detected, proceeding without");
+
+        }
+        telemetry.update();
+
+        if (useFlywheel){
+            flywheel.setDirection(DcMotorSimple.Direction.REVERSE);
+            flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+        if (useDrivetrain){
+            frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+    }
+
+
+}
